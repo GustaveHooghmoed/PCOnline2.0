5bf7cd860fb13f1b0f1f7efbe562cefd9a441476
melerpe <melerpe.habbozz@live.nl>
2017-08-29 17:56:56 +0200

    Fixed email naam!
    Nu zegt hij 'Activeer je PCO account'

--- includes/PCO_API.php
+++ includes/PCO_API.php
@@ -365,7 +365,7 @@ class user {
         $name = user::getNameByUUID($mysqli, $uuid);
 
         $to = $email;
-        $subject = "Feedback";
+        $subject = "Activeer je PCO account";
         $htmlContent = '
         <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
         <html xmlns="http://www.w3.org/1999/xhtml">
