230187a6be52b668642c87606dc50a4c785b7413
Daníque de Jong <danique.de.jong2000@gmail.com>
2017-09-29 12:16:24 +0200

    Update connectdb.php

--- includes/connectdb.php
+++ includes/connectdb.php
@@ -4,12 +4,6 @@ $username="root";
 $password="";
 $db_name="pc";
 
-/*$host="localhost";
-$username="root";
-$password="---;
-$db_name="parkcraft";*/
-
-
 $mysqli = mysqli_connect($host, $username, $password) or die(mysqli_connect_error());
 mysqli_select_db($mysqli, $db_name) or die(mysqli_connect_error());
 ?>
